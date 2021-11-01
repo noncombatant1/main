9b61735d76ef733485c5a6f9d587df7ceb81685a
noncombatant1 <dubovetsaleksey@gmail.com>
2021-11-01 16:50:35 +0600

    определение числа

---
+++ lesson2.3/lesson2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double chislo = Convert.ToDouble(Console.ReadLine());
+
+            if (chislo % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
