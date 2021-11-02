fb7b53fd4f6d70bfd61cf0e60dc9827d5c40f2a5
noncombatant1 <dubovetsaleksey@gmail.com>
2021-11-02 15:51:06 +0600

    дождливая зима

--- lesson2.2/lesson2.2/Program.cs
+++ lesson2.2/lesson2.2/Program.cs
@@ -12,48 +12,57 @@ namespace lesson2._2
         {
             Console.WriteLine("Введите номер текущего месяца");
             int numberofmounth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите среднюю температуру текущего месяца");
+            double midleTemperature = Convert.ToDouble(Console.ReadLine());
             string mouthTitle = "";
 
-            switch (numberofmounth)
+            if (midleTemperature > 0 && (numberofmounth == 1 || numberofmounth == 2 || numberofmounth == 12))
             {
-                case 1:
-                    mouthTitle = "Январь";
-                    break;
-                case 2:
-                    mouthTitle = "Февраль";
-                    break;
-                case 3:
-                    mouthTitle = "Март";
-                    break;
-                case 4:
-                    mouthTitle = "Апрель";
-                    break;
-                case 5:
-                    mouthTitle = "Май";
-                    break;
-                case 6:
-                    mouthTitle = "Июнь";
-                    break;
-                case 7:
-                    mouthTitle = "Июль";
-                    break;
-                case 8:
-                    mouthTitle = "Август";
-                    break;
-                case 9:
-                    mouthTitle = "Сентябрь";
-                    break;
-                case 10:
-                    mouthTitle = "Октябрь";
-                    break;
-                case 11:
-                    mouthTitle = "Ноябрь";
-                    break;
-                case 12:
-                    mouthTitle = "Декабрь";
-                    break;
+                Console.WriteLine("Дождливая зима");
             }
-            Console.WriteLine("Текущий месяц - " + mouthTitle);
+            else
+            {
+                switch (numberofmounth)
+                {
+                    case 1:
+                        mouthTitle = "Январь";
+                        break;
+                    case 2:
+                        mouthTitle = "Февраль";
+                        break;
+                    case 3:
+                        mouthTitle = "Март";
+                        break;
+                    case 4:
+                        mouthTitle = "Апрель";
+                        break;
+                    case 5:
+                        mouthTitle = "Май";
+                        break;
+                    case 6:
+                        mouthTitle = "Июнь";
+                        break;
+                    case 7:
+                        mouthTitle = "Июль";
+                        break;
+                    case 8:
+                        mouthTitle = "Август";
+                        break;
+                    case 9:
+                        mouthTitle = "Сентябрь";
+                        break;
+                    case 10:
+                        mouthTitle = "Октябрь";
+                        break;
+                    case 11:
+                        mouthTitle = "Ноябрь";
+                        break;
+                    case 12:
+                        mouthTitle = "Декабрь";
+                        break;
+                }
+                Console.WriteLine("Текущий месяц - " + mouthTitle);
+            }
             Console.Read();
         }
     }
